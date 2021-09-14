49f3058f53787c8a9a1002ef3781f493c9ddc91d
Andrew Nutt <anutt@paypal.com>
2021-09-14 11:08:23 -0700

    Added login function

--- src/AcmeBank.java
+++ src/AcmeBank.java
@@ -10,42 +10,45 @@ import java.util.Scanner;
 public class AcmeBank {
 
 
-    public class userLogin {
-        public void run() throws FileNotFoundException {
-            Scanner s = new Scanner(new File("customers.txt"));
-            Scanner keyboard = new Scanner(System.in);
-            String firstName = s.nextLine();
-            String lastName = s.nextLine();
-            String passWord = s.nextLine();
+    //ToDo create a method for user login
+    //I will need the following: account ID and password
+    //if the username and password exist inside the BankData.bankCustomers then show the menu options such that
+    //make a deposit
+    //make a withdrawal
+    //get balance
+    //also I need to know from what to account (checking/savings)
+    //if time permits let the user transfer money between accounts (checking to savings/savings to checking)
+    //if time permits let the user transfer between different users
+    //if the account does not exist ask the user to enter the account ID and password again.
+    //Create write file in BankData
 
-            String userInput = keyboard.nextLine();
-            String passWordInput = keyboard.nextLine();
-
-            if (userInput.equals(firstName + lastName) && passWordInput.equals(passWord)) {
-                System.out.println("Welcome " + firstName);
-            } else {
-                System.out.println("Invalid username and/or password");
-            }
+    public static void userLogin(int accountNumber, String passWord) {
+        AccountHolder accountHolder = BankData.bankCustomers.stream().filter(
+                account -> account.getAccountNumber() == accountNumber
+        ).findFirst().orElse(null);
+        if (accountHolder.getAccountNumber() == accountNumber && accountHolder.getPassWord().equals(passWord)) {
+            System.out.println("Welcome " + accountHolder.getFirstName());
+        } else {
+            System.out.println("Incorrect account number or password");
         }
     }
-        //ToDo create a method for user login
-        //I will need the following: account ID and password
-        //if the username and password exist inside the BankData.bankCustomers then show the menu options such that
-        //make a deposit
-        //make a withdrawal
-        //get balance
-        //also I need to know from what to account (checking/savings)
-        //if time permits let the user transfer money between accounts (checking to savings/savings to checking)
-        //if time permits let the user transfer between different users
-        //if the account does not exist ask the user to enter the account ID and password again.
 
     public static void main(String[] args) throws IOException {
         BankData.readFile("C:\\Users\\anutt\\Desktop\\paypal-sei\\unit1\\java-project-1\\src\\customers.txt");
 
-        for(int i = 0; i < BankData.bankCustomers.size(); i++){
-            System.out.println(BankData.bankCustomers.get(i));
+        //for (int i = 0; i < BankData.bankCustomers.size(); i++) {
+            //System.out.println(BankData.bankCustomers.get());
+
+            Scanner accountscanner = new Scanner(System.in);
+            Scanner passwordscanner = new Scanner(System.in);
+            System.out.println("Enter Account Number:");
+            Integer accountNumber = accountscanner.nextInt();
+            System.out.println("Enter Password:");
+            String passWord = passwordscanner.nextLine();
+
+            AcmeBank.userLogin(accountNumber, passWord);
 
         }
-    }
 
-}
+    }
+//}
